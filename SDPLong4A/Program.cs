279dd400d7e216b138e5c59153da7afbb24ba471
Long Le <olole005@edu.xamk.fi>
2019-10-06 14:33:27 +0300

    Add project files.

---
+++ SDPLong4A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDPLong4A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
