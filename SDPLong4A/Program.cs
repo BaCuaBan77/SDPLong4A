86e1344b708bc902f1d7ed1f2c5f9747250784fe
Long Le <olole005@edu.xamk.fi>
2019-10-06 16:27:22 +0300

    Hello changes

--- SDPLong4A/Program.cs
+++ SDPLong4A/Program.cs
@@ -6,7 +6,7 @@ namespace SDPLong4A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
             Console.Read();
         }
     }
